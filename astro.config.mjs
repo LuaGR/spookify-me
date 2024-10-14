d5644a5e419b0e561177e50d602a86af70e3ebc4
LuaAguirre <renatoag98@gmail.com>
2024-10-13 20:32:07 -0500

    add background-effect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  experimental: {
+    contentLayer: true
+  },
   integrations: [tailwind()]
 });
