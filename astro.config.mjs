ed086e6e3fe2ca704434f5db5612c757268d215a
LuaAguirre <renatoag98@gmail.com>
2024-10-23 00:42:39 -0500

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   experimental: {
     contentLayer: true
   },
-  integrations: [tailwind()]
+
+  integrations: [tailwind()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
