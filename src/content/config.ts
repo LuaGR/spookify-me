adbd1347b9ad91970b888120b9fc82af64d4fe50
LuaAguirre <renatoag98@gmail.com>
2024-10-23 00:30:00 -0500

    update assets

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,7 @@ import { cldAssetsLoader } from 'astro-cloudinary/loaders'
 
 const images = defineCollection({
   loader: cldAssetsLoader({
-    limit: 4,
-    folder: 'lua-images'
+    limit: 4
   })
 })
 
